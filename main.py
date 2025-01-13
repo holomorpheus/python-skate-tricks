c424914577f95f8847bdd19e8bfeffc07c253864
Lukas <lukas.henke.berlin@gmx.de>
2025-01-13 22:02:44 +0100

    added the showcase of the README to the main.py

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ from flip_animation import animate_fliptrick
 from homotopy_animation import animate_homotopy
 from spherical_plot import sphere_flip_plot
 from spherical_plot import sphere_homotopy_plot
+
+# Showcase
+
+## List
+list_fliptricks(complete = True)
+list_homotopies()
+
+## Plot and save
+sphere_flip_plot('varialkickflip', save=True, dirname='images')
+sphere_homotopy_plot(('varialkickflip','inwardheelflip'), save=True, dirname='images')
+
+## Animate and save
+animate_fliptrick('varialkickflip', save=True, fps=24, dirname='videos')
+animate_homotopy(('ollie','doublekickflip'))
